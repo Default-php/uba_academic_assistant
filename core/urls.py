92eda93e5dd4e41db3b2bd49531f8403abdbf13b
Default-php <alfonsogonzalezarellano22@gmail.com>
2025-06-19 16:31:39 -0400

    Feat: configure router and register all core viewsets

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'subjects', views.SubjectViewSet)
+router.register(r'inscriptions', views.InscriptionViewSet)
+router.register(r'evaluations', views.EvaluationViewSet)
+router.register(r'grades', views.GradeViewSet)
+router.register(r'resources', views.ConsultationResourceViewSet)
+router.register(r'interactions', views.AgentInteractionViewSet)
 
 urlpatterns = [
     path('', views.api_root, name='api-root'),
+    path('', include(router.urls)),  # 👈 Incluye rutas generadas automáticamente
 ]
